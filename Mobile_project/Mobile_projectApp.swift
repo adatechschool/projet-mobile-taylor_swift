ca8b808d25c35e97b8283102833f1a8189326336
b-khadija <khadija.bayssir@gmail.com>
2023-08-30 17:26:03 +0200

    Initial Commit

---
+++ Mobile_project/Mobile_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_projectApp.swift
+//  Mobile_project
+//
+//  Created by kady on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
