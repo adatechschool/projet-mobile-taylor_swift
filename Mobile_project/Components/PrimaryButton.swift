1ac77513599f04cdfa039f9da0b79b739207dc13
b-khadija <khadija.bayssir@gmail.com>
2023-09-04 17:03:13 +0200

    Added logo app & components folder

---
+++ Mobile_project/Components/PrimaryButton.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryButton.swift
+//  Mobile_project
+//
+//  Created by kady on 04/09/2023.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton()
+    }
+}
