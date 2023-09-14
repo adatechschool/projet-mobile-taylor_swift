5bb5b1604bc8209ca4eea18ae1fc832a8545f8f2
b-khadija <khadija.bayssir@gmail.com>
2023-09-14 17:27:16 +0200

    Install packages & connect to DB

--- Back/main.go
+++ Back/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"Back/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
-	fmt.Println("Hello, world ...")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
