fe4c27a27943633309267555765ac9ec4f2372ee
b-khadija <khadija.bayssir@gmail.com>
2023-09-18 12:13:55 +0200

    Create table & CRUD

--- Back/main.go
+++ Back/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Back/controllers"
 	"Back/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -14,11 +15,11 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/posts", controllers.PostsCreate)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
